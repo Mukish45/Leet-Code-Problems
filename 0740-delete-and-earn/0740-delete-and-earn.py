d1b8f2be74f74a6565c6381099759659533e235a
Mukish S <73186875+Mukish45@users.noreply.github.com>
2023-01-18 22:05:03 +0530

    Time: 57 ms (90.22%), Space: 15.4 MB (46.74%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.py
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -2,7 +2,6 @@ class Solution(object):
     def deleteAndEarn(self, nums):
         count = Counter(nums)
         a, b = 0, 0
-        print(max(nums))
         for x in range(min(nums), max(nums)+1):
             a, b = b, max(count[x] * x + a, b)
         return b
